abcd3269fbe2b5d64ef87308cf2139bfce25c694
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2024-09-30 00:09:16 +0500

    homepage ready

--- lib/BottomNavigationBar.dart
+++ lib/BottomNavigationBar.dart
@@ -1,54 +1,112 @@
-
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/get_instance.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:one_look/pages/home/logic/HomeLogic.dart';
 
 Container buildCustomNavigationBar() {
+  final controller = Get.put(HomelogicController());
+
   return Container(
     height: 80.0, // Set the desired height here
-    child: BottomNavigationBar(
+    child: Obx(() => BottomNavigationBar(
+      currentIndex: controller.bottomMenuIndex.value, // Use .value to access the RxInt value
+      onTap: (index) {
+        controller.changeIndex(index); // Change index when a tab is tapped
+      },
       items: [
         BottomNavigationBarItem(
           icon: CustomBottomNavBarItem(
-            imagePath: 'assets/images/menu home.png', // Update with your image path
-            label: '',
+            imagePath: controller.bottomMenuIndex.value == 0
+                ? 'assets/images/menu home.png'
+                : 'assets/images/menu homeinactive.png',
           ),
-          label: '', // Leave empty if the label is handled in the custom widget
+          label: '',
+          activeIcon: Container(
+            decoration: BoxDecoration(
+              color: Colors.blue.withOpacity(0.2), // Light blue background
+              shape: BoxShape.circle,
+            ),
+            padding: EdgeInsets.all(10), // Adds space around the icon
+            child: Image.asset(
+              'assets/images/menu home.png',
+              width: 25, // Adjust the size as needed
+              height: 25,
+            ),
+          ),
         ),
         BottomNavigationBarItem(
           icon: CustomBottomNavBarItem(
-            imagePath: 'assets/images/menu activity.png', // Update with your image path
-            label: '',
+            imagePath: controller.bottomMenuIndex.value == 1
+                ? 'assets/images/menu activity_Active.png'
+                : 'assets/images/menu activity.png',
           ),
-          label: '', // Leave empty if the label is handled in the custom widget
+          label: '',
+          activeIcon: Container(
+            decoration: BoxDecoration(
+              color: Colors.blue.withOpacity(0.2), // Light blue background
+              shape: BoxShape.circle,
+            ),
+            padding: EdgeInsets.all(10), // Adds space around the icon
+            child: Image.asset(
+              'assets/images/menu activity.png',
+              width: 25, // Adjust the size as needed
+              height: 25,
+            ),
+          ),
         ),
         BottomNavigationBarItem(
           icon: CustomBottomNavBarItem(
-            imagePath: 'assets/images/menu progress.png', // Update with your image path
-            label: '',
+            imagePath: controller.bottomMenuIndex.value == 2
+                ? 'assets/images/menu progress.png'
+                : 'assets/images/menu progress_active.png',
           ),
-          label: '', // Leave empty if the label is handled in the custom widget
+          label: '',
+          activeIcon: Container(
+            decoration: BoxDecoration(
+              color: Colors.blue.withOpacity(0.2), // Light blue background
+              shape: BoxShape.circle,
+            ),
+            padding: EdgeInsets.all(10), // Adds space around the icon
+            child: Image.asset(
+              'assets/images/menu progress.png',
+              width: 25, // Adjust the size as needed
+              height: 25,
+            ),
+          ),
         ),
         BottomNavigationBarItem(
           icon: CustomBottomNavBarItem(
-            imagePath: 'assets/images/menu settings.png', // Update with your image path
-            label: '',
+            imagePath: controller.bottomMenuIndex.value == 3
+                ? 'assets/images/menu settings.png'
+                : 'assets/images/menu settings_Active.png',
           ),
-          label: '', // Leave empty if the label is handled in the custom widget
+          label: '',
+          activeIcon: Container(
+            decoration: BoxDecoration(
+              color: Colors.blue.withOpacity(0.2), // Light blue background
+              shape: BoxShape.circle,
+            ),
+            padding: EdgeInsets.all(10), // Adds space around the icon
+            child: Image.asset(
+              'assets/images/menu settings.png',
+              width: 25, // Adjust the size as needed
+              height: 25,
+            ),
+          ),
         ),
         // Add other items...
       ],
-      // Add other BottomNavigationBar properties like currentIndex, onTap, etc.
-    ),
+    )),
   );
 }
 
 class CustomBottomNavBarItem extends StatelessWidget {
   final String imagePath;
-  final String label;
 
   const CustomBottomNavBarItem({
     Key? key,
     required this.imagePath,
-    required this.label,
   }) : super(key: key);
 
   @override
@@ -61,7 +119,7 @@ class CustomBottomNavBarItem extends StatelessWidget {
           width: 27, // Adjust the size as needed
           height: 27,
         ),
-       ],
+      ],
     );
   }
 }
---
+++ lib/pages/home/logic/HomeLogic.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+class HomelogicController extends GetxController  {
+  var bottomMenuIndex = 0.obs; // RxInt
+
+  void changeIndex(int index) {
+    bottomMenuIndex.value = index; // Update using .value
+    print(bottomMenuIndex.value);
+  }
+}
